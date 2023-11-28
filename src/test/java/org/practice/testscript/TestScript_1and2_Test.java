ca6d1fc00cfaf4925d8473b2f1f82e33c2178ca3
CodeRoverAbhay <abhay.shaby@gmail.com>
2023-11-28 10:58:36 +0530

    Pushing the Framework structure

---
+++ src/test/java/org/practice/testscript/TestScript_1and2_Test.java
@@ -0,0 +1,15 @@
+package org.practice.testscript;
+
+import org.testng.annotations.Test;
+
+public class TestScript_1and2_Test {
+	@Test(groups = "smoke")
+	public void ts_1_test() {
+		System.out.println("Test Script 1");
+	}
+
+	@Test(groups = "regression")
+	public void ts_2_test() {
+		System.out.println("Test Script 2");
+	}
+}
---
+++ src/test/java/org/practice/testscript/TestScript_3and4_Test.java
@@ -0,0 +1,15 @@
+package org.practice.testscript;
+
+import org.testng.annotations.Test;
+
+public class TestScript_3and4_Test {
+	@Test(groups = "smoke")
+	public void ts_3_test() {
+		System.out.println("Test Script 3");
+	}
+
+	@Test(groups = "regression")
+	public void ts_4_test() {
+		System.out.println("Test Script 4");
+	}
+}
