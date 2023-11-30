eb77ca2398d2974b9f31473bb8f2c35ae5052626
coderoverabhay <abhay.shaby@gmail.com>
2023-11-30 13:26:59 +0530

    7

--- src/test/java/org/hrm/browser/LaunchBrowserTest.java
+++ src/test/java/org/hrm/browser/LaunchBrowserTest.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LaunchBrowserTest {
 	WebDriver driver;
-	
+
 	@Test
 	public void launchBrowserTest() {
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://www.bing.com");
 		System.out.println("Driver is launched.");
